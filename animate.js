8dab54356e23c71134ab82d10298c4dd4f0f1aab
DeathWired <37221707+DeathWired@users.noreply.github.com>
2022-12-30 23:11:14 -0800

    Adding Baffle to website and adding margins

--- animate.js
+++ animate.js
@@ -1,23 +1,24 @@
 const textList = [
-    '<i class="fab fa-instagram fa-lg fa-fw"></i>',
-    '<i class="fab fa-github fa-lg fa-fw"></i>',
-    '<i class="fab fa-twitter fa-lg fa-fw"></i>',
-    "<i class='fas fa-chess-pawn fa-lg fa-fw'></i>",
-  ];
-  const linklist = [
-    "https://www.instagram.com/deafwired/",
-    "https://github.com/DeathWired",
-    "https://twitter.com/Deaf_Wired",
-    "https://www.chess.com/member/deafwired",
-  ];
-  const cycle = document.getElementById("cycle");
-  let w = 0;
-  const cycleText = () => {
-    cycle.setAttribute("href", linklist[w]);
-    cycle.setAttribute("class", "fadeOut");
-    cycle.innerHTML = textList[w];
-    w = ++w % textList.length;
-  };
-  cycleText();
-  cycle.addEventListener("animationiteration", cycleText);
-  
+  '<i class="fab fa-instagram fa-lg fa-fw"></i>',
+  '<i class="fab fa-github fa-lg fa-fw"></i>',
+  '<i class="fab fa-twitter fa-lg fa-fw"></i>',
+  "<i class='fas fa-chess-pawn fa-lg fa-fw'></i>",
+];
+const linklist = [
+  "https://www.instagram.com/deafwired/",
+  "https://github.com/DeathWired",
+  "https://twitter.com/Deaf_Wired",
+  "https://www.chess.com/member/deafwired",
+];
+const cycle = document.getElementById("cycle");
+let w = 0;
+const cycleText = () => {
+  cycle.setAttribute("href", linklist[w]);
+  cycle.setAttribute("class", "fadeOut");
+  cycle.setAttribute("style", "color:orange;");
+  cycle.setAttribute("target", "_blank");
+  cycle.innerHTML = textList[w];
+  w = ++w % textList.length;
+};
+cycleText();
+cycle.addEventListener("animationiteration", cycleText);
---
+++ baffle.js
@@ -0,0 +1,8 @@
+const text = baffle("#baffle");
+text.set({
+  characters: "▓░█ />▒<1234567890#$%!+",
+  speed: 130,
+});
+text.start();
+text.reveal(5000);
+text.stop();
